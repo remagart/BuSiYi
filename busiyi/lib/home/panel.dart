42f90720ee387a09fb116d445fd97643899178a2
remagart <jfmamjjasond418@gmail.com>
2021-10-28 11:42:49 +0800

    panel

--- busiyi/lib/home/panel.dart
+++ busiyi/lib/home/panel.dart
@@ -6,12 +6,32 @@ class PanelView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      width: 250,
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: const [
-          PanelItem(),
-          PanelItem(),
-          PanelItem(),
+        children: [
+          PanelItem(
+            title: '超商購買',
+            myIcon: Icons.event_busy,
+          ),
+          Container(
+            width: 1,
+            height: 40,
+            color: Colors.black12,
+          ),
+          PanelItem(
+            title: '銀行轉帳',
+            myIcon: Icons.bakery_dining,
+          ),
+          Container(
+            width: 1,
+            height: 40,
+            color: Colors.black12,
+          ),
+          PanelItem(
+            title: '常見問題',
+            myIcon: Icons.question_answer,
+          ),
         ],
       ),
     );
@@ -19,20 +39,44 @@ class PanelView extends StatelessWidget {
 }
 
 class PanelItem extends StatelessWidget {
-  const PanelItem({Key? key}) : super(key: key);
+  String title;
+  IconData myIcon;
+  PanelItem({Key? key, this.title = '', this.myIcon = Icons.ac_unit})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Column(
-        children: const [
-          Icon(
-            Icons.store,
-            size: 30,
-          ),
-          Text('超商購買')
-        ],
+    return GestureDetector(
+      onTap: () {
+        onClicked(title);
+      },
+      child: Container(
+        child: Column(
+          children: [
+            Icon(
+              myIcon,
+              size: 30,
+            ),
+            Text(title)
+          ],
+        ),
       ),
     );
+  }
+
+  onClicked(title) {
+    switch (title) {
+      case '超商購買':
+        print('123');
+        break;
+      case '銀行轉帳':
+        print('456');
+        break;
+      case '常見問題':
+        print('789');
+        break;
+      default:
+        print('000');
+    }
   }
 }
